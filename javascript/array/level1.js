d7ff963bb437e2d4062226ca4e82f775fdc01ab9
ntsh5446 <nikandel1111@gmail.com>
2024-04-23 09:22:50 +0545

    reverse

---
+++ javascript/array/level1.js
@@ -0,0 +1,11 @@
+
+
+const arr =[3,5,2]
+//Q2 reverse the array and console log
+//expected output [2,5,3]
+console.log(arr.reverse(" "))
+
+
+///Q3 join the array and console log
+// expected output 352
+console.log( arr.join(""))
