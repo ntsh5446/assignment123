2b2956d07f9a0a3caa1c62fbeab4df503b031061
ntsh5446 <nikandel1111@gmail.com>
2024-04-23 09:07:32 +0545

    lesser

--- javascript/basic/operator.js
+++ javascript/basic/operator.js
@@ -1,12 +1,12 @@
-//Q1 create two constants, add them assign to a new constants
-const x = 10
-const y = 20
-const z = x + y
-console.log(z)
+
+// generate a random number between 0 and 5, if it is lesser than 3
+//console.log "lesser than 3" else "Greater"
+
+const randomNumber = Math.floor(Math.random() * 6)
+if (randomNumber < 3){
+    console.log("lesser than 3")
+} else {
+    console.log ("Greater")
+}
 
 
-
-//Check below code, output should be false by comparing. console.log the answer
-const a = 10
-const b = '10'
-console.log(a == b)
