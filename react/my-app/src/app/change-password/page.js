c3db8096f7f3ea22ae05bc057c83dae9a4587f94
ntsh5446 <nikandel1111@gmail.com>
2024-04-30 15:57:57 +0545

    page

---
+++ react/my-app/src/app/change-password/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>change password</div>
+  )
+}
+
+export default page
---
+++ react/my-app/src/app/page.js
@@ -0,0 +1,15 @@
+const Home = () => {
+  ///Jsx
+  return (
+    <div> 
+      <div className="Greet" >hii</div>
+     <input name="username" placeholder="Enter your username"></input><br></br>
+     <input name="email" placeholder="Enter your Email"></input>
+     <input type="password" placeholder="Enter your username"></input>
+      <li> apple</li>
+      <li> banana</li>
+      <li> orange</li>
+    </div> 
+  )
+}
+export default Home
