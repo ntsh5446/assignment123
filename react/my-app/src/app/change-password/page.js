8463b2587082e39f8f11dd3522fafd5de2228b30
ntsh5446 <nikandel1111@gmail.com>
2024-05-03 12:28:07 +0545

    add react

--- react/my-app/src/app/change-password/page.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = () => {
-  return (
-    <div>change password</div>
-  )
-}
-
-export default page
--- react/my-app/src/app/login/page.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const login = () => {
-  return (
-    <div>login</div>
-  )
-}
-
-export default page
